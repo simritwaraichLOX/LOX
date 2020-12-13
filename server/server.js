76a242b8d8b5ea6da077ef6733dfc72d3f3e7e34
simrit-waraich <simrit.waraich@chariotprojects.com>
2020-12-13 17:34:26 -0500

    Updated server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const db = require("./lib/db.js");
+const bcrypt = require("bcryptjs");
+const uuid = require("uuid");
+const fs = require("fs");
+const start_clean = false;
 
 const corsOptions = {
   origin: 'http://ec2-3-96-168-166.ca-central-1.compute.amazonaws.com',
@@ -21,6 +26,49 @@ app.use(express.static("/public"));
 // const routes = require("./routes/router");
 // app.use("/api", routes);
 
+(async () => {
+  if (!start_clean) return;
+
+  bcrypt.hash("Testing123!", 10, (err, hash) => {
+    // Hash Password
+    if (err) {
+      console.log(err);
+    } else {
+      var uid = uuid.v4(); // Unique ID
+      db()
+        .query(
+          `INSERT INTO users (unique_id, first_name, last_name, username, email, password) VALUES (?, ?, ?, ?, ?, ?)`,
+          [uid, "The", "Admin", "the.admin", "the.admin@gmail.com", hash]
+        )
+        .then((result) => {
+          return {
+            data: result[0],
+          };
+        })
+        .then((result) => {
+          if (!fs.existsSync("./uploads/" + result.data.insertId)) {
+            console.log("No Existing Directory. Creating Directory.");
+            fs.mkdirSync("./uploads/" + result.data.insertId);
+          }
+
+          db()
+            .query(`INSERT INTO user_roles (user_id, role_id) VALUES (?,?)`, [
+              result.data.insertId,
+              1,
+            ])
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+}).call();
+
+
+
 require("./routes/router")(app);
 
 // database init method could be developed
@@ -30,3 +78,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
