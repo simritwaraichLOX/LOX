1f7198880a6197f12519bddf657908fd0b5be308
simrit-waraich <simrit.waraich@chariotprojects.com>
2020-12-16 17:59:36 -0500

    added pdf support

--- server/middleware/helpers.js
+++ server/middleware/helpers.js
@@ -1,6 +1,6 @@
 const imageFilter = function (req, file, cb) {
   // Accept images only
-  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF|pdf)$/)) {
     req.fileValidationError = "Only image files are allowed!";
     return cb(new Error("Only image files are allowed!"), false);
   }
